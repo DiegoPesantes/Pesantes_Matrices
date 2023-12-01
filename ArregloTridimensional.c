114bc70c0bd0a2f3ecafaaba02483cc631a0e903
Diego Pesantes <Diego.pesantes@udla.edu.ec>
2023-11-30 20:16:56 -0500

    Pedimos los datos al usuario

---
+++ ArregloTridimensional.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int x, y, z;
+
+    // Pedir datos al usuario
+    printf("Ingrese la dimensión x: ");
+    scanf("%d", &x);
+
+    printf("Ingrese la dimensión y: ");
+    scanf("%d", &y);
+
+    printf("Ingrese la dimensión z: ");
+    scanf("%d", &z);
+    return 0;
+}
